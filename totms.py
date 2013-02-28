723df9a4405081f4131dd312c60b1a1bf97c241b
Chris Collins <christopher.collins@duke.edu>
2013-02-27 21:18:08 -0500

    added section for examining the entrance, some error handling and some internal functions

--- totms.py
+++ totms.py
@@ -10,28 +10,19 @@
 import os
 from sys import exit
 
+# Global vars that need to be set at the beginning of the game
+choice_count = 0
+
+# Start the game
+
 
 def startgame(firsttime):
     if  firsttime == "yes":
         os.system('clear')
-        print """
-                  ______ __         ______              __
-                 /_  __// /  ___   /_  __/___   __ _   / /
-                  / /  / _ \/ -_)   / /  / _ \ /  ' \ / _ \\
-                 /_/  /_//_/\__/   /_/   \___//_/_/_//_.__/
-
-  *****************************  of the  ************************************
- __  __  _   _  ____    ____    ___   ____    ____  _____  ____   _____  ____
-|  \/  || | | ||  _ \  / ___|  / _ \ |  _ \  / ___|| ____||  _ \ | ____||  _ \\
-| |\/| || | | || | | | \___ \ | | | || |_) || |    |  _|  | |_) ||  _|  | |_) |
-| |  | || |_| || |_| |  ___) || |_| ||  _ < | |___ | |___ |  _ < | |___ |  _ <
-|_|  |_| \___/ |____/  |____/  \___/ |_| \_\ \____||_____||_| \_\|_____||_| \_\\
-
-        """
     else:
         print "\t*****\n"
         print "\tBack again, eh?\n\n"
-        raw_input("Continue...")
+        cont(0)
 
     print """
     \tBefore you stands the entrance to the tomb of the Mud Sorcerer,
@@ -39,11 +30,12 @@ def startgame(firsttime):
     since it's discovery fourty years ago, leaving the rooms looted and
     vandalized.  Yet so little was found in the last resting place of such
     a powerful man that rumors abound of hidden entrances and fabulous wealth
-    lost in the tomb.\n\n\tAnd so you stand, excited to be searching for your
-    fortune at last...\n\n
+    lost in the tomb.\n
+    \tAnd so you stand, excited to be searching for your
+    fortune at last...\n
     """
 
-    print "What do you do?\n"
+    dowhat()
     print "1. Enter the Tomb"
     print "2. Look around first"
     print "3. Chicken out and go home instead..."
@@ -54,18 +46,18 @@ def startgame(firsttime):
 
         if next == "1":
             print "\nYou decide to enter the Tomb.\n"
-            raw_input("Continue...")
+            cont(0)
             main_entrance()
         elif next == "2":
             print "\nYou decide to look around first.\n"
-            raw_input("Continue...")
-            lazy_storyteller(startgame, 'no')
+            cont(0)
+            entrance_look()
         elif next == "3":
             print "\nYou decide to Chicken out and go home instead...\n"
-            raw_input("Continue...")
+            cont(0)
             chicken_out()
         else:
-            print "That's not a choice."
+            badchoice(next, choice_count)
 
 
 def main_entrance():
@@ -73,10 +65,51 @@ def main_entrance():
     lazy_storyteller(startgame, 'no')
 
 
+def entrance_look():
+    os.system('clear')
+    print """
+    \tThe entrance to the Tomb of the Mud Sorcerer is unassuming for one who
+    once ruled the better part of the kingdom with an iron fist of terror.\n
+    \tDug into the hillside is a small entrance - an opening a head shorter
+    than you stand. A rough-cut stone lintel supports the weight of the hill
+    above. Nearby lies a broken stone disk that once covered the entrance.
+    A few low shrubs and bushes struggle to live in the poor soil.\n
+    \tThe moon waxes large through the thin clouds that smear the sky.
+    It's cold light is enough for you to make out details of the
+    scene.\n
+    To one side, a slumbering lazy storyteller reclines under the branches
+    of one of the larger bushes.\n
+    """
+
+    dowhat()
+    print "1. Enter the tomb"
+    print "2. Wake up the storyteller"
+    print "3. Chicken out and run away..."
+    print ""
+
+    while True:
+        next = raw_input("> ")
+
+        if next == "1":
+            print "\nYou enter the Tomb.\n"
+            cont(0)
+            main_entrance()
+        elif next == "2":
+            print "\nYou decide to prod the storyteller with your boot.\n"
+            cont(0)
+            wake_storyteller()
+        elif next == "3":
+            print "\nYou decide to run away like a scared little girl...\n"
+            cont(0)
+            chicken_out()
+        else:
+            print "That's not a choice."
+
+
 def lazy_storyteller(go_where, opt):
     os.system('clear')
     print """
-    \tRight now the story teller is lazy.  (You can see him
+    \tRight now the storyteller is lazy.  (You can see him
     sleeping off to the side of the entrance, next to some bushes).
     Otherwise, he's not thought of anything for you to see yet.\n
     """
@@ -84,6 +117,33 @@ def lazy_storyteller(go_where, opt):
     print "\tMight as well start out at the beginning, again.\n"
 
     go_where(opt)
+
+
+def wake_storyteller():
+    os.system('clear')
+    print """
+    \tYou approach the storyteller.  The man is huge - easily well over
+    six feet tall, and several hundred pounds.  An impressive beard graces
+    his face.\n
+    \tAs you reach forward with your boot, the storyteller suddenly wakes
+    and screams!\n
+    \tBrillian light pours from his eyes and mouth and shines from behind
+    the prone figure.  A sound like none you've ever heard roars in your
+    ears.\n
+    \tThe storyteller slowly rises from the ground to float above you,
+    arms and legs outstretched, with more of the brillian fire pouring
+    from palms and soles.  Around you stones pull free from the earth and
+    rise into the air.  The ground cracks and splinters and the struggling
+    local flora ignites in flame.\n
+    \tThe noise grows - louder and louder until you feel you can no longer
+    deal with the pain.\n
+    \tThe floating figure gazes upon you, and slowly raises one hand
+    to point you directly at you.  The world is suddenly filled with
+    the light of a thousand suns and then...\n
+    """
+
+    cont(0)
+    dead("The storyteller vaporizes you for your arrogance.")
 
 
 def chicken_out():
@@ -98,10 +158,71 @@ def chicken_out():
     exit(0)
 
 
+def dowhat():
+    print "What do you do?\n"
+
+
+def cont(i):
+    if i == 0:
+        raw_input("Continue...")
+    elif i == 1:
+        raw_input("")
+    else:
+        die("Error: Out of Bounds variable in function 'cont'.")
+
+
+def badchoice(choice, count):
+    print "\t%r is not a choice.\n" % choice
+    choices = [
+        "\tPerhaps you should try reading for a change.\n",
+        "\tLook - see the numbers?  Pick one, genius.\n",
+        "\tYou're just screwing with me now, aren't you?\n",
+        "\tCome on, don't be a jerk.\n",
+        "Fine, screw  you too."]
+
+    my_choice = choices[count]
+
+    global choice_count
+
+    if choice_count <= 3:
+        print my_choice
+        choice_count += 1
+    else:
+        cont(0)
+        dead(my_choice)
+
+
 def dead(why):
-    print why, "\tYou have died."
-    print "\tThank you for playing."
+    os.system('clear')
+    print ""
+    print "\t", why, "You are dead.\n"
+    print "\tOH NOES!\n"
+    cont(1)
+    os.system('clear')
+    print ""
+    print "\tThank you for playing.\n\n"
     exit(0)
 
+
+def die(why):
+    print "Error:", why
+    exit(1)
+
+def logo():
+    print """
+                  ______ __         ______              __
+                 /_  __// /  ___   /_  __/___   __ _   / /
+                  / /  / _ \/ -_)   / /  / _ \ /  ' \ / _ \\
+                 /_/  /_//_/\__/   /_/   \___//_/_/_//_.__/
+
+  *****************************  of the  ************************************
+ __  __  _   _  ____    ____    ___   ____    ____  _____  ____   _____  ____
+|  \/  || | | ||  _ \  / ___|  / _ \ |  _ \  / ___|| ____||  _ \ | ____||  _ \\
+| |\/| || | | || | | | \___ \ | | | || |_) || |    |  _|  | |_) ||  _|  | |_) |
+| |  | || |_| || |_| |  ___) || |_| ||  _ < | |___ | |___ |  _ < | |___ |  _ <
+|_|  |_| \___/ |____/  |____/  \___/ |_| \_\ \____||_____||_| \_\|_____||_| \_\\
+
+                                                                      v0.1
+   """
 
 startgame("yes")
